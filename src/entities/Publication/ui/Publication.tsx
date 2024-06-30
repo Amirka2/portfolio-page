70fd8862560f8a98d14d323811eaa40f63bdfd5d
Amir <far20022002213@gmail.com>
2024-06-30 17:55:34 +0500

    publications feat: added publication entity, added publications with mock data

---
+++ src/entities/Publication/ui/Publication.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import * as SC from "./Publication.styles";
+
+interface PublicationProps {
+  photo: string;
+  text: string;
+  year: string;
+}
+
+export const Publication = ({ photo, text, year }: PublicationProps) => {
+  return (
+    <SC.Wrapper>
+      <SC.Image src={photo} />
+      <SC.Info>
+        <SC.Text>{text}</SC.Text>
+        <SC.Text>
+          <b>{year}</b>
+        </SC.Text>
+      </SC.Info>
+    </SC.Wrapper>
+  );
+};

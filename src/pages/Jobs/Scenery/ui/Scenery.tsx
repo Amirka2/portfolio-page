b7303b8662a6efc25a71366723f24ffc3c2548cd
Amir <far20022002213@gmail.com>
2024-07-02 04:42:31 +0500

    api feat: added resource setting in i18n in jobs categories pages

--- src/pages/Jobs/Scenery/ui/Scenery.tsx
+++ src/pages/Jobs/Scenery/ui/Scenery.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 
+import { prepareWorksDescriptions, setResources } from "@entities/Jobs";
 import { Gallery } from "@widgets/Gallery";
 
 import * as GS from "../../ui";
@@ -7,20 +8,29 @@ import * as GS from "../../ui";
 import { useSceneries } from "../api";
 import * as SC from "./Scenery.styles";
 
+const CATEGORY_KEY = "JobsScenery";
+
 export const Scenery = () => {
   const { data, isPending, isError } = useSceneries();
 
+  useEffect(() => {
+    if (data) {
+      const preparedResources = prepareWorksDescriptions(data);
+      setResources(preparedResources, CATEGORY_KEY);
+    }
+  }, [data]);
+
   if (isPending) {
-    return <>loading</>
+    return <>loading</>;
   }
 
   if (isError) {
-    return <>error</>
+    return <>error</>;
   }
 
   return (
     <GS.Wrapper>
-      <Gallery photos={data} />
+      <Gallery photos={data} category={CATEGORY_KEY} />
     </GS.Wrapper>
   );
 };

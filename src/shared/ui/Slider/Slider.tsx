966ea749904142d62f74597a8c42c2369fe63365
Amir <far20022002213@gmail.com>
2024-07-02 20:09:41 +0500

    api chore: fixed styles

--- src/shared/ui/Slider/Slider.tsx
+++ src/shared/ui/Slider/Slider.tsx
@@ -1,24 +1,42 @@
-import React, { PropsWithChildren } from "react";
+import React, { ReactNode } from "react";
 import SlickSlider, { Settings } from "react-slick";
 
-import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 
 import * as SC from "./Slider.styles";
 
 const BASE_SETTINGS: Settings = {
   dots: true,
-  speed: 500,
+  speed: 200,
   slidesToShow: 1,
   slidesToScroll: 1,
   fade: true,
   infinite: true,
+  swipeToSlide: true,
 };
 
-interface SliderProps extends PropsWithChildren {}
+interface SliderProps extends Settings {
+  children: ReactNode;
+  activeSlide?: number;
+}
 
-export const BaseSlider = ({ children }: SliderProps) => {
-  return <SlickSlider {...BASE_SETTINGS}>{children}</SlickSlider>;
+export const BaseSlider = ({
+  activeSlide = 0,
+  children,
+  ...restProps
+}: SliderProps) => {
+  return (
+    <SlickSlider
+      {...BASE_SETTINGS}
+      {...restProps}
+      ref={(slider) => {
+        slider?.slickGoTo(activeSlide);
+      }}
+    >
+      {children}
+    </SlickSlider>
+  );
 };
 
 export const Slider = Object.assign(BaseSlider, {

8a24627f218ece0aa3cafe37dd397661d70ccfab
Amir <far20022002213@gmail.com>
2024-07-05 00:21:48 +0500

    refactor: added cross icon

--- src/shared/ui/Icons/index.ts
+++ src/shared/ui/Icons/index.ts
@@ -3,3 +3,4 @@ export { Whatsapp as WhatsappIcon } from './Whatsapp';
 export { Arrow as ArrowIcon } from './Arrow';
 export { Burger as BurgerIcon } from './Burger';
 export { MiniArrow as MiniArrowIcon } from './MiniArrow';
+export { Cross as CrossIcon } from './Cross';
